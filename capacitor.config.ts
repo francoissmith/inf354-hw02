4969a3c48dca13edfa91938a3787604c979778bf
Francois Smith <90509905+francoissmith@users.noreply.github.com>
2023-04-25 20:25:42 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'u19314486_HW02',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
